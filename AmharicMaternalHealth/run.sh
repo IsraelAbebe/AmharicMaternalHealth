4d1004eeda59ddbf32c5b8ba3384c62476d572b4
Israel Abebe <se.israel.abebe@gmail.com>
2026-03-29 22:23:36 +0200

    Refactor run.sh for model evaluations and outputs
    Updated the model list and modified evaluation commands to include new output paths and options.

--- AmharicMaternalHealth/run.sh
+++ AmharicMaternalHealth/run.sh
@@ -1,27 +1,50 @@
-MODELS=("google/gemma-3-1b-it" "meta-llama/Llama-3.2-1B-Instruct"  "google/gemma-3-12b-it" "meta-llama/Llama-3.1-8B-Instruct" "CohereLabs/c4ai-command-r7b-12-2024" "CohereLabs/c4ai-command-r-v01")
+
+MODELS=( "meta-llama/Llama-3.2-1B-Instruct" "google/gemma-3-12b-it" "google/gemma-3-27b-it" "meta-llama/Llama-3.1-70B-Instruct" "meta-llama/Llama-3.1-8B-Instruct" "McGill-NLP/AfriqueQwen-14B" "google/medgemma-27b-it")
 
 for MODEL_NAME in "${MODELS[@]}"; do
     echo "Running evaluations for model: $MODEL_NAME"
 
     # Multiple Choice
-    lm_eval --model hf \
-            --model_args pretrained=$MODEL_NAME \
+    CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} lm_eval --model hf \
+            --model_args pretrained=$MODEL_NAME,parallelize=True \
             --tasks AmharicMaternalHealth_amharic_ap1,AmharicMaternalHealth_amharic_ap2,AmharicMaternalHealth_amharic_ap3,AmharicMaternalHealth_amharic_ep1,AmharicMaternalHealth_amharic_ep2,AmharicMaternalHealth_amharic_ep3,AmharicMaternalHealth_english_ep1,AmharicMaternalHealth_english_ep2,AmharicMaternalHealth_english_ep3 \
-            --device cuda:0 \
             --batch_size 1 \
-            --limit 2 \
-            --output_path output/choice-$MODEL_NAME \
+            --output_path output-amharicsotryqa/choice-$MODEL_NAME \
             --verbosity DEBUG \
-            --log_samples
+            --log_samples \
+            --apply_chat_template
 
     # Generation
-    lm_eval --model hf \
-            --model_args pretrained=$MODEL_NAME \
+    CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} lm_eval --model hf \
+            --model_args pretrained=$MODEL_NAME,parallelize=True \
             --tasks AmharicMaternalHealth_amharic_ap1_gen,AmharicMaternalHealth_amharic_ap3_gen,AmharicMaternalHealth_amharic_ep1_gen,AmharicMaternalHealth_amharic_ep2_gen,AmharicMaternalHealth_amharic_ep3_gen,AmharicMaternalHealth_english_ep1_gen,AmharicMaternalHealth_english_ep2_gen,AmharicMaternalHealth_english_ep3_gen \
-            --device cuda:0 \
             --batch_size 1 \
-            --limit 2 \
-            --output_path output/generation-$MODEL_NAME \
+            --output_path output-amharicsotryqa/generation-$MODEL_NAME \
             --verbosity DEBUG \
-            --log_samples
+            --log_samples \
+            --apply_chat_template
+    cp -r output-amharicsotryqa/ /home/azime/localization-evaluation/.
+
 done
+
+
+
+
+
+# |AmharicMaternalHealth_amharic_ap1                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/amh/amh_p1.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_amharic_ap1_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/amh/amh_p1.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_amharic_ap2                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/amh/amh_p2.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_amharic_ap3                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/amh/amh_p3.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_amharic_ap3_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/amh/amh_p3.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_amharic_ep1                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/eng/amh_p1.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_amharic_ep1_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/eng/amh_p1.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_amharic_ep2                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/eng/amh_p2.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_amharic_ep2_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/eng/amh_p2.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_amharic_ep3                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/eng/amh_p3.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_amharic_ep3_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/eng/amh_p3.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_english_ep1                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/eng/eng_p1.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_english_ep1_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/eng/eng_p1.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_english_ep2                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/eng/eng_p2.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_english_ep2_gen                                                 |lm_eval/tasks/AmharicMaternalHealth/generation/eng/eng_p2.yaml                                                                                                  |generate_until       |
+# |AmharicMaternalHealth_english_ep3                                                     |lm_eval/tasks/AmharicMaternalHealth/choice/eng/eng_p3.yaml                                                                                                      |multiple_choice      |
+# |AmharicMaternalHealth_english_ep3_gen   
